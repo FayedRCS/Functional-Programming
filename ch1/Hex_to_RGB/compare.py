932f9869e865f8d61a42d01fdb225bfc192cc032
fayed <fayed_rana@outlook.com>
2024-09-11 23:59:01 +0200

    solved with help

--- ch1/Hex_to_RGB/compare.py
+++ ch1/Hex_to_RGB/compare.py
@@ -1,14 +1,12 @@
 def hex_to_rgb(hex_color):
-
     if not is_hexadecimal(hex_color) or len(hex_color) != 6:
         raise Exception("not a hex color string")
-
-    r = int(hex_color[:2], base = 16)
-    g = int(hex_color[2:4], base = 16)
-    b = int(hex_color[4:], base = 16)
-
+    r = int(hex_color[:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:], 16)
     return r, g, b
-    
+
+
 def is_hexadecimal(hex_string):
     try:
         int(hex_string, 16)

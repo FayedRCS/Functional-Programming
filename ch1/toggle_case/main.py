64734e8a69c237b928ecaa60e2fe1970bfc57905
fayed <fayed_rana@outlook.com>
2024-09-11 23:10:54 +0200

    Solutions

---
+++ ch1/toggle_case/main.py
@@ -0,0 +1,14 @@
+def toggle_case(line):
+
+    if line.istitle():
+        return line.upper() + "!!!"
+
+    if line.isupper():
+
+        return line.lower().capitalize().rstrip("!")
+
+    if len(line) > 0 and line[1:].islower():
+        return line.title()
+
+    return line
+ 

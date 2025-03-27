7351d2512e7ab3f7b85db042bf75fcd767dd008d
fayed <fayed_rana@outlook.com>
2025-03-27 14:52:58 +0100

    added zip and filter solutions

--- ch2/zip/main.py
+++ ch2/zip/main.py
@@ -11,4 +11,15 @@ valid_formats = [
 
 
 def pair_document_with_format(doc_names, doc_formats):
-    pass
+    
+    #Zips each element sharing the same indices together and returns a new iterable => tuple
+    full_doc = list(zip(doc_names, doc_formats))
+
+
+    #filters through the full document, and returns each element in the tuple that shares the same file format. hence the "[1]"
+    return list(filter(lambda verify: verify[1] in valid_formats, full_doc))
+
+
+
+
+    

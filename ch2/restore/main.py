10e9fc79c8fef97039ae0371a73209e2da7642b0
fayed <fayed_rana@outlook.com>
2025-03-27 15:59:26 +0100

    restore challenge solution

--- ch2/restore/main.py
+++ ch2/restore/main.py
@@ -1,2 +1,8 @@
 def restore_documents(originals, backups):
-    pass
+
+    #easy to get tricked by not concatenating both tuple arguments. concatinate them, then map over to uppercase.
+    #Then we filter through returning only if True to NOT being a digit
+    
+    return set(filter(lambda digit: not digit.isdigit(), map(lambda upp: upp.upper(), originals + backups)))
+
+

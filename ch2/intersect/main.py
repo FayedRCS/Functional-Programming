343bbf3b8b87ce7a27e502222f73d5ba1a0f0260
fayed <fayed_rana@outlook.com>
2025-03-27 14:28:29 +0100

    intersection solution added

--- ch2/intersect/main.py
+++ ch2/intersect/main.py
@@ -1,2 +1,6 @@
 def get_common_formats(formats1, formats2):
-    pass
+  
+  #function takes in a list if integers, that needs to be converted to sets
+  ## .intersection() method returns a new set containing only elements that are in both
+
+  return set(formats1).intersection(set(formats2)) 

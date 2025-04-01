656e604a77c68ff0af27e417a6123be9519c0be2
fayed <fayed_rana@outlook.com>
2025-04-01 19:08:32 +0200

    custom commands solution

--- ch3/custom_commands/main.py
+++ ch3/custom_commands/main.py
@@ -6,19 +6,22 @@ saved_documents = {}
 
 
 def add_custom_command(commands, new_command, function):
+    commands = commands.copy()
     commands[new_command] = function
     return commands
 
 
 def add_format(formats, format):
+    formats = formats.copy()
     formats.append(format)
     return formats
 
 
 def save_document(docs, file_name, doc):
+    docs = docs.copy()
     docs[file_name] = doc
     return docs
 
 
 def add_line_break(line):
-    print(line + "\n\n")
+    return line + "\n\n"

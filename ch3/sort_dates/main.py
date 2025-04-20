f36f2744284f1571715a9e7457af48de04b4f911
fayed <fayed_rana@outlook.com>
2025-04-20 23:19:41 +0200

    sort_dates solution added w/ comments

--- ch3/sort_dates/main.py
+++ ch3/sort_dates/main.py
@@ -1,3 +1,16 @@
 def sort_dates(dates):
-    dates.sort()
+
+
+    #Nested function so that it's easier to see what index splits into year/month/day
+
+    
+    dates = sorted(dates, key=year_month_day) #key= could be replaced with 'lambda x : (x.split("-")[2], x.split("-")[0], x.split("-")[1])'
+
     return dates
+
+def year_month_day(dates):
+    year = dates.split("-")[2]
+    month = dates.split("-")[0]
+    day = dates.split("-")[1]
+    return year, month, day
+    

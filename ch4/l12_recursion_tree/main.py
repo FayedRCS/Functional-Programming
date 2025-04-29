7adf982fc9a9e71a401dc77173ea81dd78bb5ce4
fayed <fayed_rana@outlook.com>
2025-04-29 21:39:09 +0200

    current progress

--- ch4/l12_recursion_tree/main.py
+++ ch4/l12_recursion_tree/main.py
@@ -1,2 +1,25 @@
 def list_files(parent_directory, current_filepath=""):
-    pass
+
+    file_path = []
+
+    for path in parent_directory:
+        current_filepath += "/" + path
+
+        if parent_directory[path] == None:
+            file_path.append(current_filepath)
+        elif current_filepath[path]:
+            child_directory = list_files(parent_directory[path], current_filepath)
+            file_path.extend(child_directory)
+
+    return file_path
+
+
+
+test =  {
+            "Documents": {
+                "Proposal.docx": None,
+                "Report": {"AnnualReport.pdf": None, "Financials.xlsx": None},
+            } }
+
+
+print(list_files(test, ""))

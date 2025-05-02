9f3f46056a6e793038afa094972fda88b44d13cd
fayed <fayed_rana@outlook.com>
2025-05-02 22:57:19 +0200

    lesson solved, took a while...

--- ch4/l12_recursion_tree/main.py
+++ ch4/l12_recursion_tree/main.py
@@ -3,6 +3,7 @@ def list_files(parent_directory, current_filepath=""):
     file_path = []
 
     for path in parent_directory:
+        #create  a new string, can't modify the current_filepath
         new_path = current_filepath + "/" + path
 
         if parent_directory[path] == None:

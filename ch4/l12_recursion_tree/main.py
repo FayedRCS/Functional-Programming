6584ab4c67821b3e883e9f89a69b291bc637fae6
fayed <fayed_rana@outlook.com>
2025-05-02 22:56:52 +0200

    lesson solution, took 7 days...

--- ch4/l12_recursion_tree/main.py
+++ ch4/l12_recursion_tree/main.py
@@ -3,23 +3,13 @@ def list_files(parent_directory, current_filepath=""):
     file_path = []
 
     for path in parent_directory:
-        current_filepath += "/" + path
+        new_path = current_filepath + "/" + path
 
         if parent_directory[path] == None:
-            file_path.append(current_filepath)
-        elif current_filepath[path]:
-            child_directory = list_files(parent_directory[path], current_filepath)
+            file_path.append(new_path)
+        
+        else:
+            child_directory = list_files(parent_directory[path], new_path)
             file_path.extend(child_directory)
 
     return file_path
-
-
-
-test =  {
-            "Documents": {
-                "Proposal.docx": None,
-                "Report": {"AnnualReport.pdf": None, "Financials.xlsx": None},
-            } }
-
-
-print(list_files(test, ""))
